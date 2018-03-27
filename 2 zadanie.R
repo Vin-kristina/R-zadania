ad3c81d4750594f35137799b6d606fa04200d855
Vin-kristina <36107341+Vin-kristina@users.noreply.github.com>
2018-03-27 18:08:11 +0300

    Update 2 zadanie.R

--- 2 zadanie.R
+++ 2 zadanie.R
@@ -8,7 +8,7 @@ library("readr")
 tbl=read.csv("eddypro.csv", skip = 1, na =c("","NA","-9999","-9999.0"), comment=c("[")) 
 tbl=tbl[-1,] 
 tbl 
-tbl=tbl[tbl$DOY > 62 & tbl$DOY < 156,] 
+tbl=tbl[tbl$DOY > 152 & tbl$DOY < 244,] 
 tbl=tbl[tbl$daytime == FALSE,] 
 glimpse(tbl) 
 tbl = select(tbl, -(roll)) 
@@ -38,32 +38,32 @@ cor_td = cor(drop_na(tbl_numeric)) %>% as.data.frame %>% select(h2o_flux)
 cor_td 
 vars = row.names(cor_td)[cor_td$h2o_flux^2 > .1] %>% na.exclude 
 formula = as.formula(paste("h2o_flux~", paste(vars,collapse = "+"), sep="")) 
-formula 
-mod=lm(formula, data = tbl) 
-anova(mod) 
-summary(mod) 
+formula
+mod=lm(formula, data = tbl)
+anova(mod)
+summary(mod)
 formula1 = as.formula(paste("h2o_flux ~ Tau + rand_err_Tau + H + LE + rand_err_LE + h2o_flux + 
-                            rand_err_h2o_flux + co2_molar_density + co2_mole_fraction + 
-                            co2_mixing_ratio + h2o_time_lag + sonic_temperature + air_temperature + 
-                            air_density + air_molar_volume + es + RH + VPD + u_rot + 
-                            wind_speed + max_speed + u. + TKE + un_Tau + un_H + un_LE + 
-                            un_h2o_flux + u_var + v_var + w_var + w.ts_cov + w.h2o_cov + 
-                            co2 + co2.1 + flowrate")) 
-formula1 
-mod2=lm(formula1, data = tbl) 
-anova(mod2) 
-summary(mod2) 
+    rand_err_h2o_flux + co2_molar_density + co2_mole_fraction + 
+    co2_mixing_ratio + h2o_time_lag + sonic_temperature + air_temperature + 
+    air_density + air_molar_volume + es + RH + VPD + u_rot + 
+    wind_speed + max_speed + u. + TKE + un_Tau + un_H + un_LE + 
+    un_h2o_flux + u_var + v_var + w_var + w.ts_cov + w.h2o_cov + 
+    co2 + co2.1 + flowrate"))
+formula1
+mod2=lm(formula1, data = tbl)
+anova(mod2)
+summary(mod2)
 formula2 = as.formula(paste("h2o_flux ~ Tau + rand_err_Tau + H + LE + rand_err_LE + h2o_flux + 
     rand_err_h2o_flux + co2_molar_density + co2_mole_fraction + 
                             co2_mixing_ratio + h2o_time_lag + air_temperature + 
                             air_density + air_molar_volume + es + RH + VPD + u_rot + 
                             wind_speed + u. + TKE + un_Tau + un_H + un_h2o_flux + 
-                            co2")) 
-mod3=lm(formula2, data = tbl) 
-anova(mod3) 
-summary(mod3) 
+                            co2"))
+mod3=lm(formula2, data = tbl)
+anova(mod3)
+summary(mod3)
 formula3 = as.formula(paste("h2o_flux ~  H + LE + un_LE + 
-                            un_h2o_flux")) 
-mod4=lm(formula3, data = tbl) 
-anova(mod4) 
+   un_h2o_flux"))
+mod4=lm(formula3, data = tbl)
+anova(mod4)
 summary(mod4)
